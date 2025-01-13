d2421ecfbd9c3b91e5b4ee7cfa69c6831f013ea4
kdy071115 <kdy071115@naver.com>
2025-01-13 14:22:40 +0900

    초기 커밋

---
+++ src/main/java/com/kaidey/yakchatproject/YakchatProjectApplication.java
@@ -0,0 +1,14 @@
+package com.kaidey.yakchatproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakchatProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YakchatProjectApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/kaidey/yakchatproject/YakchatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaidey.yakchatproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakchatProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
