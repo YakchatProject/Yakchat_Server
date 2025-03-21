ae3f93dce481ea38e3384be448ae8e14e73b0b97
kdy071115 <kdy071115@naver.com>
2025-03-21 22:17:27 +0900

    파일 구조 모듈화로 변경(기능 증가로 인한 유지 보수)

--- src/main/java/com/kaidey/yakchatproject/domain/like/repository/LikeRepository.java
+++ src/main/java/com/kaidey/yakchatproject/domain/like/repository/LikeRepository.java
@@ -1,6 +1,6 @@
-package com.kaidey.yakchatproject.repository;
+package com.kaidey.yakchatproject.domain.like.repository;
 
-import com.kaidey.yakchatproject.entity.Like;
+import com.kaidey.yakchatproject.domain.like.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
